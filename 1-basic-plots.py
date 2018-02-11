1dd6516330ef7e3129b23af76d95cc7e96f4f127
mmejdoubi <mejdoubi2005@gmail.com>
2018-02-11 23:12:55 +0000

    commit #2

--- 1-basic-plots.py
+++ 1-basic-plots.py
@@ -1,2 +1,9 @@
 import matplotlib as plt
 import pandas as pd
+
+input_file_path = "/Users/mac/python/jack-dies/data/train.csv"
+
+df = pd.read_csv(input_file_path)
+print "\n loading CSV file Train.csv"
+df.head(10)
+df.dtypes
